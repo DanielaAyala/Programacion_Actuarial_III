7dfd5580dd33618307188e59db2ff88c9d51c8f1
DanielaAyala <dani.ayala_28@hotmail.com>
2016-09-05 09:44:24 -0500

    Uso de dput y dget
    creación de archivo, guardar y enviar a github

---
+++ data.R
@@ -0,0 +1,6 @@
+x <-
+"Programación Actuarial III"
+y <-
+structure(list(a...1 = c(-1, -1, -1, -1, -1), b = structure(c(1L, 
+1L, 1L, 1L, 1L), class = "factor", .Label = "a")), .Names = c("a...1", 
+"b"), row.names = c(NA, -5L), class = "data.frame")
---
+++ z.R
@@ -0,0 +1,1 @@
+c("progrmación", "Actuarial", "III")
