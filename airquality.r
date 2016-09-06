8fb11cc679960a24c62e2e6fa6ccf494e928a946
DanielaAyala <dani.ayala_28@hotmail.com>
2016-09-06 00:21:11 -0500

    lista
    Muestra lista del archivo airquality

--- airquality.r
+++ airquality.r
@@ -2,3 +2,6 @@
 
 head(airquality)
 dput(airquality,file="AirQuality")
+ 
+x <-"Programación_actuarial_III"
+y <-structure(list(a = 1, b = structure(1L, .Label = "a", class = "factor")), .Names = c("a","b"), row.names = c(NA, -1L), class = "data.frame")
